03c18b7664d806606a3fd70e95f9fe1661a00080
nabilaghoniii <100661882+nabilaghoniii@users.noreply.github.com>
2022-06-24 22:10:18 +0700

    Update Posting.R

--- Posting.R
+++ Posting.R
@@ -31,7 +31,7 @@ post2 <- paste0(
 )
 
 twitter_token <- rtweet::create_token(
-  app = "jualrumahbgr",
+  app = "weathworld",
   consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"), 
   consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
   access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
